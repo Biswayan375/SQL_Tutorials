b163368b74c022ce77fa7b94b382c4e401f8339a
Biswayan375 <biswayannandi1947@gamil.com>
2021-09-01 11:30:52 +0530

    Regular changes: Some extra content added on using group by clause on multiple columns and corresponding queries and concept notes are added in queries.sql and GroupBy.txt

--- queries.sql
+++ queries.sql
@@ -9,13 +9,13 @@ create table employee(
 );
 
 -- To insert values in it
-insert into employee values('E07', 'Ajay', 3000, 'Kolkata', '12-JAN-00');
-insert into employee values('E19', 'Bijay', 3219, 'Gurgaon', '01-JUL-10');
-insert into employee values('E20', 'Roy', 5270, 'Kolkata', '18-FEB-12');
-insert into employee values('E48', 'Ram', 4350, 'UP', '12-JAN-02');
-insert into employee values('E13', 'Sam', 4100, 'Mumbai', '18-MAY-07');
-insert into employee values('E14', 'Sunil', 4517, 'Gujrat', '16-JUN-1999');
-insert into employee values('E50', 'Sunil', 4300, 'Kolkata', '12-AUG-2001');
+insert into employee values('E01', 'Ajay', 3000, 'Kolkata', '12-JAN-00');
+insert into employee values('E02', 'Bijay', 3219, 'Gurgaon', '01-JUL-10');
+insert into employee values('E03', 'Roy', 5270, 'Kolkata', '18-FEB-12');
+insert into employee values('E04', 'Ram', 4350, 'UP', '12-JAN-02');
+insert into employee values('E05', 'Sam', 4100, 'Mumbai', '18-MAY-07');
+insert into employee values('E06', 'Sunil', 4517, 'Gujrat', '16-JUN-1999');
+insert into employee values('E07', 'Sunil', 4300, 'Kolkata', '12-AUG-2001');
 
 -- Finding out the name of the employee who gets the maximum salary using subquery
 select name as fullname from employee 
@@ -69,3 +69,74 @@ select * from
 select * from employee 
 where city in(select city from employee group by city 
 	having count(*) > 1)order by city;
+	
+-- To create work table where e_id is the only primary key of employee table and d_id is the only primary key of department table (The related references are in 4.Primary_Foreign_Bridge_SubQry_Alias_Distinct.txt)
+create table work(
+	eid varchar2(5),
+	did varchar2(5),
+	foreign key(eid)references employee(e_id) on delete cascade,
+	foreign key(did)references department(d_id) on delete cascade
+);
+
+-- Find out the department name where E01 works without using bridge
+select name from department
+where
+	d_id in(
+		select did from work
+		where
+			eid = 'E01'
+	);
+	
+-- Find out the department name where E01 works with bridges
+select name from work, department
+where
+	department.d_id = work.did and
+	work.eid = 'E01';
+
+-- Find out name of people who work in HR department
+select employee.name from employee, work
+where
+	work.did = (select d_id from department where name='HR') and
+	employee.e_id = work.eid;
+	
+-- Another way to do the above query
+select e.name from employee e, department d, work w
+where
+	e.e_id = w.eid and
+	w.did = d.d_id and
+	d.name = 'IT';
+
+-- Doing the same above thing with subqueries
+select name from employee
+where
+	e_id in(select eid from work
+		where did = (select d_id from department
+			where name = 'IT'
+		)
+	);
+
+-- Give out the department name of those employees who live in kolkata
+select d.name from employee e, department d, work w
+where
+	d.d_id = w.did and
+	w.eid = e.e_id and
+	e.city = 'Kolkata';
+
+-- Give out the employee details from kolkata who has assigned with atleast one department
+select * from employee
+where
+	name in(select distinct(e.name) from employee e, work w
+	where
+		e.e_id = w.eid and
+		e.city = 'Kolkata');
+		
+
+select e.e_id, e.name, count(*) as "No. of deps. assigned" from employee e, work w
+group by w.eid, e.e_id, e.name
+having
+	w.eid = e.e_id;
+	
+-- Give out the name and id of the employee along with the number of departments assigned to them
+select e.e_id, e.name, count(*) as "No. of deps. assigned" from employee e, work w where w.eid = e.e_id
+group by
+	e.e_id, e.name;
